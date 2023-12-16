04b0eb0d54cbbac8c5edd9ea5be10feb8f2a1c03
Agata Sierzega <AgataSierzega@gmail.com>
2023-12-16 14:43:25 +0000

    login,create account and forgotten pass. html

--- root/create_account.php
+++ root/create_account.php
@@ -1,5 +1,52 @@
-<?php
-echo "working";
-// or
-// print "working";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Account</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+     <!-- Custom CSS -->
+     <link rel="stylesheet" href="styles.css">
+     <title>MK Time</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">MK Time</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link disabled">Disabled</a>
+                <a class="nav-link" href="\mktime\root\login.php">Login</a>
+            </div>
+        </div>
+    </nav>
+    <br><br><br>
+    <h2><b>Create account</b></h2>
+    <form action="create_account.php" method="post">
+
+        <label for="Name">Name:</label>
+        <input type="name" id="name" name="name" placeholder="Enter name" required><br>
+
+        <label for="email">Email address:</label>
+        <input type="email" id="email" name="email" placeholder="Enter email address" required><br>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" placeholder="Enter password" required><br>
+
+        <label for="password">Repeat Password:</label>
+        <input type="password" id="password" name="password" placeholder="Repeat Password" required><br><br>
+
+        <input type="submit" value="Create Account"><br><br>
+        <hr>
+
+        <div class="already-have-account">
+            <a href="login.php">ALREADY HAVE AN ACCOUNT?</a></p>
+        </div>
+    </form>
+</body>
+</html>
--- root/login.php
+++ root/login.php
@@ -5,48 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-        }
-
-        h2 {
-            max-width: 300px;
-            margin: 50px auto 0 20%;
-            text-align: left;
-        }
-
-        form {
-            max-width: 300px;
-            margin: 50px auto 0 20%;
-        }
-
-        label {
-            display: block;
-            margin-bottom: 5px;
-        }
-
-        input {
-            width: 100%;
-            padding: 8px;
-            margin-bottom: 10px;
-        }
-
-        .forgot-password,
-        .create-account {
-            text-align: left;
-            margin-top: 10px;
-            margin-left: 0; /* Adjusted to 0 for alignment with the form */
-        }
-
-        .forgot-password a,
-        .create-account a {
-            color: black;
-            text-decoration: underline;
-        }
-    </style>
+     <!-- Custom CSS -->
+     <link rel="stylesheet" href="styles.css">
+     <title>MK Time</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -65,22 +26,23 @@
         </div>
     </nav>
     <br><br><br>
-    <h2>Login</h2>
+    <h2><b>Login</b></h2>
     <form action="login.php" method="post">
-        <label for="email">Email:</label>
+        <label for="email">Email address:</label>
         <input type="email" id="email" name="email" placeholder="Enter email address" required><br><br>
 
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" placeholder="Enter password" required><br><br>
 
         <input type="submit" value="Login"><br><br>
+        <hr>
 
         <div class="forgot-password">
             <a href="forgot_password.php">Forgot Password?</a>
         </div>
 
         <div class="create-account">
-            <p>Don't have an account?<br> <a href="create_account.php">Create Account</a></p>
+            <p>Don't have an account?<br> <a href="create_account.php">CREATE ACCOUNT</a></p>
         </div>
     </form>
 </body>
