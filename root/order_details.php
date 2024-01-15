f424baabc32d2a3275552510bce797476372d0ab
aagb1884 <a.a.g.blair@gmail.com>
2024-01-15 14:42:43 +0000

    Update order_details.php

--- root/order_details.php
+++ root/order_details.php
@@ -31,8 +31,7 @@
 	  
 	    if (mysqli_num_rows($result) > 0) {
       $row = mysqli_fetch_assoc($result);
-	  echo '	<div class="order-details"
-				<h5 class="card-title">Order # '.$row["order_id"].'</h5>
+	  echo '	<h5 class="card-title">Order # '.$row["order_id"].'</h5>
 				<p class="card-text">Order date: '.$row["order_date"].'</p>
 				<p class="card-text">Total Cost: £'.$row["total"].'</p>
 				<p class="card-text">Payment ID: '.$row["payment_id"].'</p>
@@ -48,27 +47,29 @@ $sql_items = "SELECT oc.*, p.product_name, p.product_img, p.product_price
 				echo '<div class="row">';
                 while ($row_items = mysqli_fetch_assoc($result_items)) {
                 echo '<div class="col-md-3 d-flex justify-content-center">
-					<div class="card" style="width: 18rem; margin: 5px;
-				    <p class="card-text"><strong>' . $row_items["product_name"] . '</strong></p>
-					<p class="card-text">Quantity: ' . $row_items["quantity"] . '</p>
-					<p class="card-text">Price: £' . $row_items["product_price"] . '</p>
-					<img src="' . $row_items["product_img"] . '" alt="' . $row_items["product_name"] . '" width="50" height="50">
-					</p>
-					</div>
-					</div>'; }			
+						<div class="card" style="width: 18rem; margin: 5px;">
+						<p class="card-text"><strong>' . $row_items["product_name"] . '</strong></p>
+						<p class="card-text">Quantity: ' . $row_items["quantity"] . '</p>
+						<p class="card-text">Price: £' . $row_items["product_price"] . '</p>
+						<img src="' . $row_items["product_img"] . '" alt="' . $row_items["product_name"] . '" width="50" height="50">
+						</p>
+						</div>
+						</div>
+					'; }			
             } else {
                 echo '<p class="card-text">No items found in this order.</p>
 				</div>';
-            }
+				   }
 			
     } else {
       echo "Order not found.";
     }
-
+	
     mysqli_close($link);
     ?>
-	  
+	</div>
 	</div>
    <?php include('include/footer.php'); ?>
+ 
   </body>
 </html>
