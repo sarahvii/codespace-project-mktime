a2db6281267f2a0b674e9a5642a6a1f861ef8f39
aagb1884 <a.a.g.blair@gmail.com>
2024-01-15 14:08:09 +0000

    updated order details

--- root/order_details.php
+++ root/order_details.php
@@ -38,16 +38,24 @@
 				<p class="card-text">Payment ID: '.$row["payment_id"].'</p>
 				<h6 class"card-title">Items Ordered:</h6>';
 				
-	 $sql_items = "SELECT oc.*, p.product_name FROM order_contents oc
-                          JOIN products p ON oc.product_id = p.product_id
-                          WHERE oc.order_id = $order_id";
+$sql_items = "SELECT oc.*, p.product_name, p.product_img, p.product_price 
+              FROM order_contents oc
+              JOIN products p ON oc.product_id = p.product_id
+              WHERE oc.order_id = $order_id";
             $result_items = mysqli_query($link, $sql_items);
 
             if (mysqli_num_rows($result_items) > 0) {
+				echo '<div class="row">';
                 while ($row_items = mysqli_fetch_assoc($result_items)) {
-                echo '<p class="card-text">' . $row_items["product_name"] . ' - Quantity: ' . $row_items["quantity"] . '</p>
-				</div>';
-                }
+                echo '<div class="col-md-3 d-flex justify-content-center">
+					<div class="card" style="width: 18rem; margin: 5px;
+				    <p class="card-text"><strong>' . $row_items["product_name"] . '</strong></p>
+					<p class="card-text">Quantity: ' . $row_items["quantity"] . '</p>
+					<p class="card-text">Price: £' . $row_items["product_price"] . '</p>
+					<img src="' . $row_items["product_img"] . '" alt="' . $row_items["product_name"] . '" width="50" height="50">
+					</p>
+					</div>
+					</div>'; }			
             } else {
                 echo '<p class="card-text">No items found in this order.</p>
 				</div>';
